fa6e8ee1315283f3b1847652cee4e73a4a7dbc8d
suhyun9764 <suhyun9764@naver.com>
2024-02-29 19:44:13 +0900

    Initial commit

---
+++ src/main/java/com/sparta/my_memo/MyMemoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.my_memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/my_memo/MyMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.my_memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
